74afa699bc5c26e4ccf37f25a5deae31052620ae
Jesus Nunez Xoconoxtle <jesusnunezxoco@gmail.com>
2022-03-20 21:28:34 -0500

    set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
-    
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
   );
 }
 
