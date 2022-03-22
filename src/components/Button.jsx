0847d0315fb0a5c19b3be5275aafa5a2df9c0b50
Jesus Nunez Xoconoxtle <jesusnunezxoco@gmail.com>
2022-03-22 14:23:14 -0500

    we do section done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Button({ content, bgColor, textColor }) {
   return (
-    <button className={`bg-${bgColor} text-${textColor} text-lg py-3 px-4 w-fit rounded-lg`}>
+    <button className={`bg-${bgColor} text-${textColor} text-lg py-3 px-4 w-fit rounded-lg font-bold`}>
       {content}
     </button>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import Header from "../components/header";
 import Hero from "../components/Hero";
 import headerImg from "../assets/header.png";
-import wedoImg from "../assets/wedo1.png";
+import wedoImg1 from "../assets/wedo1.png";
+import wedoImg2 from "../assets/wedo2.png"
+import wedoImg3 from "../assets/wedo3.png"
 import SectionTitle from "../components/SectionTitle";
 import data from "../data.js";
 import Impact from "../components/Impact";
@@ -29,7 +31,7 @@ function Home() {
         </section>
         {/* *IMPACT* */}
         <section className="py-16">
-          <div className="container mx-auto px-5">
+          <div className="container mx-auto px-5 ">
             <SectionTitle title={"Impact"} />
             <div className="xl:grid xl:grid-cols-3 xl:items-center xl:gap-2 md:grid md:grid-cols-1 ">
               {data.impactCards.map((impact) => (
@@ -49,8 +51,8 @@ function Home() {
           {/* what we do 1*/}
           <article className="flex flex-col-reverse xl:flex-row">
             {/* content column*/}
-            <div className="bg-lightest-purple px-12 py-16 flex-1 flex flex-col justify-center">
-              <h3 className="text-2xl mb-6 capitalize">For Educators</h3>
+            <div className="bg-lightest-purple px-12 py-16 w-full xl:w-1/2 flex flex-col justify-center">
+              <h3 className="text-2xl mb-6 capitalize">Literacy Programs</h3>
               <p className="mb-6">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut
                 metus augue. Mauris facilisis eu dui accumsan pretium. Maecenas
@@ -60,21 +62,22 @@ function Home() {
                 blandit.
               </p>
               <Button
-                content="Find Educator Resources"
+                content="Explore Literary Resources"
                 bgColor={"dark-purple"}
                 textColor={"white"}
               />
             </div>
             {/* image column*/}
-            <div className="flex items-center justify-center flex-1">
-              <img alt="What we did" src={wedoImg} className="w-full " />
+            <div className="flex items-center justify-center xl:w-1/2">
+              <img alt="What we did" src={wedoImg1} className="w-full" />
             </div>
           </article>
-           {/* what we do 2*/}
-           <article className="flex flex-col-reverse xl:flex-row-reverse">
+      
+            {/* what we do 2*/}
+            <article className="flex flex-col-reverse xl:flex-row-reverse">
             {/* content column*/}
-            <div className="bg-light-green px-12 py-16 flex-1 flex flex-col justify-center">
-              <h3 className="text-2xl mb-6 capitalize">For Educators</h3>
+            <div className="bg-light-green px-12 py-16 w-full xl:w-1/2 flex flex-col justify-center">
+              <h3 className="text-2xl mb-6 capitalize">For Care Professionals</h3>
               <p className="mb-6">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut
                 metus augue. Mauris facilisis eu dui accumsan pretium. Maecenas
@@ -90,15 +93,15 @@ function Home() {
               />
             </div>
             {/* image column*/}
-            <div className="flex items-center justify-center flex-1">
-              <img alt="What we did" src={wedoImg} className="w-full " />
+            <div className="flex flex-reverse items-center justify-center xl:w-1/2">
+              <img alt="What we did" src={wedoImg2} className="w-full" />
             </div>
           </article>
-           {/* what we do 1*/}
-           <article className="flex flex-col-reverse xl:flex-row">
-            {/* content */}
-            <div className="bg-light-yellow px-12 py-16 flex-1 flex flex-col justify-center">
-              <h3 className="text-2xl mb-6 capitalize">For Educators</h3>
+              {/* what we do 3*/}
+              <article className="flex flex-col-reverse xl:flex-row">
+            {/* content column*/}
+            <div className="bg-light-yellow px-12 py-16 w-full xl:w-1/2 flex flex-col justify-center">
+              <h3 className="text-2xl mb-6 capitalize">For Care Professionals</h3>
               <p className="mb-6">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut
                 metus augue. Mauris facilisis eu dui accumsan pretium. Maecenas
@@ -108,17 +111,19 @@ function Home() {
                 blandit.
               </p>
               <Button
-                content="Find Educator Resources"
+                content="Learn About Health Services"
                 bgColor={"dark-yellow"}
-                textColor={"black"}
+                textColor={"white"}
               />
             </div>
             {/* image column*/}
-            <div className="flex items-center justify-center flex-1">
-              <img alt="What we did" src={wedoImg} className="w-full " />
+            <div className="flex items-center justify-center xl:w-1/2">
+              <img alt="What we did" src={wedoImg3} className="w-full" />
             </div>
           </article>
+      
         </section>
+        {/* LATEST BLOGS */}
       </main>
     </>
   );
